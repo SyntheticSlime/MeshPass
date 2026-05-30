096f4cd7f2d0dbdce699ba6448531e31885cb108
Steven <syntheticsteve@gmail.com>
2026-05-29 21:23:20 -0400

    added exit and did some cleanup

--- MeshPass.py
+++ MeshPass.py
@@ -12,19 +12,21 @@ from cryptography.fernet import Fernet
 MULTICAST_UDP_IP = '224.0.0.86'
 MULTICAST_UDP_PORT = 5005
 MULTICAST_TUPLE = (MULTICAST_UDP_IP, MULTICAST_UDP_PORT)
-
-print(f'Listening for UDP packets on {MULTICAST_UDP_IP}:{MULTICAST_UDP_PORT}')
-
+MULTICAST_TTL = 4
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+MY_IP_ADDRESS = s.getsockname()[0]
+s.close()
 HEADER_LEN = 64
 #SERVER_TCP_PORT = 5050
 FORMAT = 'utf-8'
 MYHOSTNAME = socket.gethostname()
-MY_IP_ADDRESS = socket.gethostbyname(MYHOSTNAME)
-MY_TCP_PORT = 5050
-print(f"my host name: {MYHOSTNAME}")
-print(f"server ip address: {MY_IP_ADDRESS}")
+#MY_IP_ADDRESS = socket.gethostbyname(MYHOSTNAME)
+#MY_OTHER_IP_ADDRESS = socket.gethostbyaddr(MYHOSTNAME)
+MY_TCP_PORT = 5051
+
 MY_SOCKET_TUPLE = (MY_IP_ADDRESS, MY_TCP_PORT)
 CLIENT_SOCKET_TUPLE = ()
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -35,32 +37,35 @@ PARAMETERS_RESPONSE = '!PARAMETERS_RESPONSE'
 
 
 def main(cli_args):
+    print(f'Listening for UDP packets on {MULTICAST_UDP_IP}:{MULTICAST_UDP_PORT}')
+    print(f"my host name: {MYHOSTNAME}")
+    print(f"my ip address: {MY_IP_ADDRESS}")
 
     # Create a UDP socket
-    multicast_udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    multicast_udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #This line has something to do with timeouts and avoiding errors due to timeouts.
+    multicast_listen_udp_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    multicast_listen_udp_socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #This line has something to do with timeouts and avoiding errors due to timeouts.
+    #multicast_listen_udp_socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #This line has something to do with timeouts and avoiding errors due to timeouts.
     
-    multicast_udp_sock.bind(("", MULTICAST_UDP_PORT))
+    multicast_listen_udp_socket_obj.bind(MULTICAST_TUPLE)
     
-    mreq = struct.pack("4s4s",
-                       socket.inet_aton(MULTICAST_UDP_IP),
-                       socket.inet_aton("0.0.0.0"))  # see note
-    multicast_udp_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    mreq = struct.pack('4sl', socket.inet_aton(MULTICAST_UDP_IP), socket.INADDR_ANY)
+    multicast_listen_udp_socket_obj.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
-    #multicast_udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #multicast_udp_sock.bind(MULTICAST_TUPLE)
-    my_udp_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #multicast_listen_udp_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #multicast_listen_udp_socket_obj.bind(MULTICAST_TUPLE)
+    multicast_send_udp_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    multicast_send_udp_socket_obj.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
     #set up TCP server socket to listen for sync requests
     my_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create TCP socket
     my_socket_obj.bind(MY_SOCKET_TUPLE)              # bind socket to address and port
 
     #server_socket_obj.listen()
-    #multicast_udp_sock_list = [multicast_udp_sock]
-    #multicast_udp_sock_tup = tuple({multicast_udp_sock})
+    #multicast_listen_udp_socket_obj_list = [multicast_listen_udp_socket_obj]
+    #multicast_listen_udp_socket_obj_tup = tuple({multicast_listen_udp_socket_obj})
     thread_udp = threading.Thread(target=listen_for_multicasts,
-                              args=(multicast_udp_sock,)
+                              args=(multicast_listen_udp_socket_obj,)
                              )
     thread_udp.start()
     #my_socket_obj_list = [my_socket_obj]
@@ -74,12 +79,17 @@ def main(cli_args):
     while True:
         command = input().upper()
         if command == 'FIND':
-            send_multicast(my_udp_socket_obj)
+            send_multicast(multicast_send_udp_socket_obj)
+        elif command == 'EXIT':
+            multicast_listen_udp_socket_obj.close()
+            multicast_send_udp_socket_obj.close()
+            my_socket_obj.close()
+            exit()
 
-def listen_for_multicasts(multicast_udp_sock):
+def listen_for_multicasts(multicast_listen_udp_socket_obj):
     while True:
         # Receive data from the socket
-        data, partner_socket_tuple = multicast_udp_sock.recvfrom(1024)
+        data, partner_socket_tuple = multicast_listen_udp_socket_obj.recvfrom(1024)
         message = data.decode('utf-8')
         if message == MY_IP_ADDRESS:
             print(f'{partner_socket_tuple}: This is me.')
@@ -90,11 +100,11 @@ def listen_for_multicasts(multicast_udp_sock):
         my_socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         my_socket_obj.connect(partner_socket_tuple)
 
-def send_multicast(my_udp_socket_obj):
+def send_multicast(multicast_send_udp_socket_obj):
     message = f'{MY_IP_ADDRESS}'
     try:
         print(f'{MULTICAST_UDP_IP}, {MULTICAST_UDP_PORT}, {message}')
-        my_udp_socket_obj.sendto(message.encode(),
+        multicast_send_udp_socket_obj.sendto(message.encode(),
                              (MULTICAST_UDP_IP, MULTICAST_UDP_PORT)
                             )
         print(f'Sent UDP packet to {MULTICAST_UDP_IP}:'
