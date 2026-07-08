24ab7020335eba9f284083347def02107553d6ae
SyntheticSlime <syntheticsteve@gmail.com>
2026-07-08 16:58:15 -0400

    no changes

--- MeshPass.py
+++ MeshPass.py
@@ -10,6 +10,10 @@ It also allows one-way syncing of selected records with other users.
 import socket
 import struct
 import threading
+import platform
+import re
+import subprocess
+
 from cryptography.hazmat.primitives            import hashes
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf   import HKDF
@@ -24,9 +28,58 @@ RCV_BFR_SZ          = 1024
 
 
 HEADER_LEN    = 64
+LINUX         = 'Linux'
+WINDOWS       = 'Windows'
+SYSTEM        = platform.system()
 FORMAT        = 'utf-8'
 MYHOSTNAME    = socket.gethostname()
-MY_IP_ADDRESS = socket.gethostbyname(MYHOSTNAME)
+MY_IP_ADDRESS = None
+if SYSTEM == LINUX:
+    data = subprocess.run(['ip', 'addr'], capture_output=True).stdout.decode(FORMAT)
+    print(f'{type(data)}')
+    pat = r"inet ((?:192|172|10)\.\d+\.\d+\.\d+)"
+    MY_IP_ADDRESS = re.search(pat, data).group(0)
+elif SYSTEM == WINDOWS:
+    data = subprocess.run(['ipconfig'], capture_output=True).stdout.decode(FORMAT)
+    data_by_line = data.split(' ')
+    title = None
+    title_pattern = r"(\w[^:]*)"
+    ip_pattern = r'IPv4 Address[ .]*: (\d+\.\d+\.\d+\.\d+)'
+    default_gateway_pattern = r'Default Gateway[ .]*: (\d+\.\d+\.\d+\.\d+)'
+    title_match = None
+    ip_match = None
+    default_gateway_found = False
+    for line in data_by_line:
+        title_match = re.search(title_pattern, line)
+        if title_match:
+            title = title_match.group(0)
+            default_gateway_found = False
+            MY_IP_ADDRESS = None
+            pass
+        
+        ip_match = re.search(ip_pattern, line)
+        if ip_match:
+            MY_IP_ADDRESS = ip_match.group(1)
+
+        if re.search(default_gateway_pattern, line):
+            default_gateway_found = True
+
+        if title and MY_IP_ADDRESS and default_gateway_found:
+            break
+else:
+    print(f'Unknown system.')
+    return 0
+
+
+
+    pat = r"(^(?:\w+)[^\:]*)[\s\S]*(?:IPv4 Address)[ .:]*(\d+\.\d+\.\d+\.\d+)"
+    match = re.search(pat, data)
+    title = match.group(1)
+    print(f'{title}')
+    ip_address = match.group(2)
+    print(f'{ip_address}')
+
+print(f'MY_IP_ADDRESS: {MY_IP_ADDRESS}')
 MY_TCP_PORT   = 5051
 UNICAST_UDP_PORT = 5052
 UNICAST_TUPLE = (MY_IP_ADDRESS, UNICAST_UDP_PORT)
@@ -62,11 +115,16 @@ def main(cli_args):
     mreq = struct.pack('4sl',
                        socket.inet_aton(MULTICAST_UDP_IP),
                        socket.INADDR_ANY)
+    if SYSTEM == LINUX:
+        multicast_listen_udp_socket_obj.bind(MULTICAST_TUPLE)
+    else:
+        multicast_listen_udp_socket_obj.bind(('', MULTICAST_UDP_PORT))
+
     multicast_listen_udp_socket_obj.setsockopt(socket.IPPROTO_IP,
                                                socket.IP_ADD_MEMBERSHIP,
                                                mreq)
 
-    multicast_listen_udp_socket_obj.bind(('', 0))
+    
 
     unicast_socket_obj = socket.socket(socket.AF_INET,
                                        socket.SOCK_DGRAM,
