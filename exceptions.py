77b42106b2a49b7b47d3b61e746f1ab942529b5d
Ron Bandes <RBandes@c9n.com>
2026-07-22 21:02:52 -0400

    Fix UserError exception so that it can be caught in try/except stmt.

--- exceptions.py
+++ exceptions.py
@@ -4,10 +4,7 @@
 import sys  # exit()
 
 class _MPException (Exception):
-    def __init__(self, message=''):
-        if len(message) > 0:
-            sys.exit(message)
-        return
+    pass
 
 class ProgrammerError (_MPException):
     def __init__ (self, message=''):
@@ -17,4 +14,3 @@ class ProgrammerError (_MPException):
 class UserError (_MPException):
     def __init__ (self, message=''):
         super().__init__(message)  # for ancestor class
-        sys.exit('User error not handled by program - program terminating.')
